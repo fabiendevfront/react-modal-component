c705f830da1bdd036a1688e98305c51ebe7ac32f
Fabien Dev <fabiendevfront@gmail.com>
2023-03-07 20:21:11 +0100

    Add modal overlay, modal design

--- src/lib/components/ReactModalComponent.jsx
+++ src/lib/components/ReactModalComponent.jsx
@@ -3,14 +3,16 @@ import "../styles/main.scss";
 const ReactModalComponent = ({ displayModal, title, children }) => {
     return (
         <div className="react-modal-component">
-            <div className="react-modal-component__container">
-                <div className="react-modal-component__head">
-                    <h3 className="react-modal-component__title">{title}</h3>
-                    <button className="react-modal-component__btn" onClick={() => displayModal(false)}>
-                        X
-                    </button>
+            <div className="react-modal-component__overlay">
+                <div className="react-modal-component__container">
+                    <div className="react-modal-component__head">
+                        <h3 className="react-modal-component__title">{title}</h3>
+                        <span className="react-modal-component__btn" onClick={() => displayModal(false)}>
+                            x
+                        </span>
+                    </div>
+                    {children}
                 </div>
-                {children}
             </div>
         </div>
     );
